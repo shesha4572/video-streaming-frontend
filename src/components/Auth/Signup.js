e8b1a50006c6e2b314bae171e6842591020d6267
KunalPasad <pasadk9@gmail.com>
2023-09-27 06:16:01 +0530

    initial

---
+++ src/components/Auth/Signup.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Signup = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('/api/signup', formData);
+            console.log(response.data); // Handle success
+        } catch (error) {
+            console.error(error); // Handle error
+        }
+    };
+
+    return (
+        <div>
+            <h2>Sign Up</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    value={formData.name}
+                    onChange={handleChange}
+                />
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    );
+};
+
+export default Signup;
