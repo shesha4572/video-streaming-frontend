ed5672c750785b9a780a89dbf6e227717862b0ab
pratikshatrixie <pratikshatrixie@gmail.com>
2023-11-19 13:33:36 +0530

    fileId

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
             }
           />
           <Route
-            path="/play/:internalField"
+            path="/play/:internalFileId"
             element={
               <div>
                 <Header />
