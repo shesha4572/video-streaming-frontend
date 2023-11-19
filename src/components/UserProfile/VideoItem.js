fe4c7894ed4a4aec1855e3f122735ca497bc41ac
pratikshatrixie <pratikshatrixie@gmail.com>
2023-11-19 18:32:17 +0530

    profileredirect

--- src/components/UserProfile/VideoItem.js
+++ src/components/UserProfile/VideoItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './VideoItem.css';
+import { Link } from 'react-router-dom';
 
 const VideoItem = ({ video }) => {
   return (
     <div className="video-item">
     <ol>
     <br />
+    <Link to={`/play/${video.internalFileId}`}>
       <img src={video.thumbnailLink} alt={video.title} />
+    </Link>
       <div className="video-info">
         <h3>Title: {video.title}</h3>
         <p>Description: {video.desc}</p>
