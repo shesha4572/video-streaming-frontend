b0017ded3eaa1f23e8af1d8438b18ed40d5f86a2
pratikshatrixie <pratikshatrixie@gmail.com>
2023-11-19 16:47:06 +0530

    getusername

--- src/components/Search/VideoRow.js
+++ src/components/Search/VideoRow.js
@@ -3,13 +3,14 @@ import './VideoRow.css';
 import { Link } from 'react-router-dom';
 
 function VideoRow({ title, views, timestamp, channel, description, image, internalFileId }) {
+  console.log('VideoRow Props:', { title, views, timestamp, channel, description, image, internalFileId });
+
   return (
     
     <div className='videoRow'>
       <Link to={`/play/${internalFileId}`}>
         <img src={image} alt="" />
       </Link>
-
       <div className="videoRow_text">
         <h3>{title}</h3>
         <p className='videoRow_headline'>
@@ -24,4 +25,3 @@ function VideoRow({ title, views, timestamp, channel, description, image, intern
 }
 
 export default VideoRow;
-
