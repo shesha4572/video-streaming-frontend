d6f31d13ce747307d1eb99da75620f824d48b20d
pratikshatrixie <pratikshatrixie@gmail.com>
2023-11-22 08:16:23 +0530

    profile

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import VideoList from './VideoList'; 
 import './UserProfile.css'
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBTypography, MDBBtn, MDBCardTitle } from 'mdb-react-ui-kit';
 
 
 const UserProfile = ({ userData }) => {
@@ -11,44 +10,33 @@ const UserProfile = ({ userData }) => {
 
   const { name, displayName, createdOn, uploadedVideos } = userData;
 
-  function formatDate(timestamp) {
-    const date = new Date(timestamp);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${day}-${month}-${year}`;
-  }
-
   return (
-    <div className="user-profile">
-      <div className="vh-100" style={{ backgroundColor: '#f2f2f2' }}>
-  <MDBContainer className="container py-5 h-100 d-flex align-items-center justify-content-center" style={{ width: '500px' }}>
-    <MDBRow className="justify-content-center">
-      <MDBCol md="12" xl="8">
-        <MDBCard style={{ backgroundColor: 'white'}}>
-          <MDBCardBody className="d-flex align-items-center">
-            <div className="me-4">
-              <MDBCard style={{ backgroundColor: 'white'}}>
-                <MDBCardBody>
-                  
-                </MDBCardBody>
-              </MDBCard>
+      <div className="user-profile">
+      <div className="vh-100 d-flex align-items-center justify-content-center" style={{ backgroundColor: '#f2f2f2' }}>
+        <div className="container py-5">
+          <div className="id-card mx-auto">
+            <div className="id-card-left">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                className="rounded-circle"
+                alt="Profile"
+                style={{ width: '120px', height: '120px' }}
+              />
             </div>
-            <div className="text-center">
-            <MDBTypography tag="h4" className="mb-1">{name}</MDBTypography>
-            <MDBCardText className="mb-1">
-              {displayName}
-            </MDBCardText>
-            <MDBCardText className="small">
-              Joined On: {formatDate(createdOn)}
-            </MDBCardText>
+            <div className="id-card-right">
+              <div className="id-card-row">
+                <strong>Name:</strong> {name}
+              </div>
+              <div className="id-card-row">
+                <strong>Display Name:</strong> {displayName}
+              </div>
+              <div className="id-card-row">
+                <strong>Joined On:</strong> {createdOn}
+              </div>
+            </div>
           </div>
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-    </MDBRow>
-  </MDBContainer>
-</div>
+        </div>
+      </div>
 
 
 
